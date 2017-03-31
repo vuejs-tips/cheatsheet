06448266799268c84e1cdb32497a1582f2a4f678
Marcos Neves <marcos.neves@gmail.com>
2017-03-31 02:49:59 -0300

    Refactor css and add more APIs

--- src/build.rb
+++ src/build.rb
@@ -8,11 +8,10 @@ require 'erb'
 # and copy <ul class="menu-root"> to downloaded-menu.html
 doc = Nokogiri::HTML open(__dir__ + '/downloaded-menu.html')
 root = doc.at_css('.menu-root')
-
 root.css('li:last-child').last.remove # remove the last two li
 root.css('li:last-child').last.remove
 
-modifiers = IO.read(__dir__ + '/modifiers.xml') # extra panels
+extras = IO.read(__dir__ + '/extras.xml') # extra panels
 
 html = root.to_s
 html = html.gsub(/<\/li>\s+<ul/m, "\n<ul") # remove closing li
@@ -26,7 +25,24 @@ html = html.gsub(/<\/li>\s+<ul/m, "\n<ul") # remove closing li
            .gsub('>Directives<', '><small>html</small> Directives<')
            .gsub(/>(renderError|model|productionTip|performance|provide \/ inject|vm\.\$props)</, '>\\1 <var>2.2</var><')
            .gsub(/>(v-else-if|vm\.\$scopedSlots)</, '>\\1 <var>2.1</var><')
-           .gsub(/<\/ul><\/li>\Z/m, modifiers) # append modifiers
+           .gsub(/<\/ul><\/li>\Z/m, extras) # append extras
+
+doc = Nokogiri::HTML html
+doc.at_css('.menu-root > li:nth-child(13)').add_next_sibling("
+<li class='directives hooks'><a>Directives <small>Hooks</small></a>
+  <ul>
+    <li><a href='https://vuejs.org/v2/guide/custom-directive.html#Hook-Functions'>bind</a></li>
+    <li><a href='https://vuejs.org/v2/guide/custom-directive.html#Hook-Functions'>inserted</a></li>
+    <li><a href='https://vuejs.org/v2/guide/custom-directive.html#Hook-Functions'>update</a></li>
+    <li><a href='https://vuejs.org/v2/guide/custom-directive.html#Hook-Functions'>componentUpdated</a></li>
+    <li><a href='https://vuejs.org/v2/guide/custom-directive.html#Hook-Functions'>unbind</a></li>
+  </ul>
+</li>
+")
+# move "instance properties" after "Lifecycle Methods"
+doc.at_css('.menu-root > li:nth-child(12)').add_next_sibling(doc.at_css('.menu-root > li:nth-child(9)').remove)
+doc.at_css('.menu-root > li:nth-child(2)').add_next_sibling(doc.at_css('.menu-root > li:nth-child(5)').remove)
+html = doc.to_s
 
 style = File.read(__dir__ + '/style.css')
 script = File.read(__dir__ + '/script.js')
